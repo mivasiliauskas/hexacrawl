1bf73fe8d3e06002fc856d0137acddea6ad14028
Mindaugas Vasiliauskas <mm.vasiliauskas@gmail.com>
2019-02-27 23:42:47 +0200

    Added random sprites

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -2,6 +2,15 @@
 
 public class HexCell : MonoBehaviour
 {
+    public Sprite[] characters;
+
+    static System.Random random = new System.Random();
+
+    void Awake()
+    {
+        GetComponentInChildren<SpriteRenderer>().sprite = characters[random.Next(characters.Length)];
+    }
+
     public Color color;
 
     public HexCoordinates coordinates;
