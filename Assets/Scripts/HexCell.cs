104966708b6d2d728727b011fc2204c8e454e215
Mindaugas Vasiliauskas <mm.vasiliauskas@gmail.com>
2019-02-28 17:26:01 +0200

    No sax

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -16,28 +16,11 @@ public class HexCell : MonoBehaviour
 
     public Entity entity;
 
-    AudioSource audioSource;
-
-    Transform sprite;
-
-    float speed = 5.97f;
-    float delta = 0.35f;
     int index;
 
     void Awake()
     {
-        audioSource = GameObject.Find("Audio Source").GetComponent<AudioSource>();
-        sprite = transform.Find("Sprite");
         index = random.Next(characters.Length);
         GetComponentInChildren<SpriteRenderer>().sprite = characters[index];
-    }
-    void Update()
-    {
-        float z = (((audioSource.time + delta) / (audioSource.clip.length + delta)) * 100 % speed) / speed;
-        if (index < 50){
-            sprite.localPosition = new Vector3(0, 1, z * 2);
-        } else {
-            sprite.rotation = Quaternion.Euler(90, 0, z * 15);
-        }
     }
 }
